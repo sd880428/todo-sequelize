2d1bffa1c0ce8a2d2e5375915c795af4b4c7c0b8
sd880428 <sd660428@gmail.com>
2023-02-01 02:43:54 +0800

    add CRUD featrue

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -1,13 +1,59 @@
 const express = require('express')
-const route = express.Router()
+const router = express.Router()
 const db = require('../../models/')
 const Todo = db.Todo
 
-route.get('/:id', (req, res) => {
+router.get('/create', (req, res) => {
+  res.render('create')
+})
+
+router.post('/', (req, res) => { //增
+  const name = req.body.name
+  const UserId = req.user.id
+  return Todo.create({ name, UserId })
+    .then(() => res.redirect('/'))
+    .catch(error => console.error(error))
+})
+
+router.get('/:id', (req, res) => { //查
   const id = req.params.id
-  return Todo.findByPk(id)
+  const UserId = req.user.id
+  return Todo.findOne({ raw: true, nest: true, where: { id, UserId } })
     .then(todo => res.render('detail', { todo: todo.toJSON() }))
     .catch(error => console.log(error))
 })
 
-module.exports = route
+router.get('/:id/edit', (req, res) => {
+  const id = req.params.id
+  const UserId = req.user.id
+  return Todo.findOne({ raw: true, nest: true, where: { id, UserId } })
+    .then((todo) => {
+      console.log(todo)
+      res.render('edit', { todo })
+    })
+    .catch(error => console.log(error))
+})
+
+router.put('/:id', (req, res) => { //改
+  const id = req.params.id
+  const UserId = req.user.id
+  const { name, isDone } = req.body
+  return Todo.findOne({ raw: true, nest: true, where: { id, UserId } })
+    .then((todo) => {
+      todo.name = name
+      todo.isDone = isDone === 'on'
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.log(error))
+})
+
+router.delete('/:id', (req, res) => { //刪
+  const id = req.params.id
+  const UserId = req.user.id
+  return Todo.destroy({ raw: true, nest: true, where: { id, UserId } })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+module.exports = router
